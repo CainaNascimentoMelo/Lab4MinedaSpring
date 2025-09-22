ecd5a1458c820ae578eb58cb4d9c8f87290657ab
Cainã Melo <163151125+CainaNascimentoMelo@users.noreply.github.com>
2025-09-22 13:20:16 -0300

    adicionar arquivos

---
+++ ProjetoLab4Mineda/src/main/java/br/caina/mineda/fatec/ProjetoLab4Mineda/ProjetoLab4MinedaApplication.java
@@ -0,0 +1,13 @@
+package br.caina.mineda.fatec.ProjetoLab4Mineda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLab4MinedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLab4MinedaApplication.class, args);
+	}
+
+}
---
+++ ProjetoLab4Mineda/src/test/java/br/caina/mineda/fatec/ProjetoLab4Mineda/ProjetoLab4MinedaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.caina.mineda.fatec.ProjetoLab4Mineda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLab4MinedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
